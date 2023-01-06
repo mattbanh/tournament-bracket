a2d872109b1df9dbff146d7996e5abfc03414556
Matthew Banh <mattbanh92@gmail.com>
2023-01-05 23:03:43 -0800

    created form to add player names. when submitted, bracket will appear

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
+// Lessons:
+// If you use uuid, during re-render it will create a new key, re-rendering any elements that contain the key
+
+import { useState } from "react";
 import "./App.css";
 import Bracket from "./components/Bracket/Bracket";
 
 function App() {
+  const [eightGroup, setEightGroup] = useState(1);
+  const [start, setStart] = useState(false);
+  const [values, setValues] = useState({
+    P1: "",
+    P2: "",
+    P3: "",
+    P4: "",
+    P5: "",
+    P6: "",
+    P7: "",
+    P8: "",
+  });
+  const players = Object.keys(values);
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+
+  const startTournament = (e) => {
+    e.preventDefault();
+    setStart(true);
+  };
+
   return (
     <section>
-      <Bracket />
+      {!start && (
+        <section>
+          <h1 className="mb-8">Start Your Tournament</h1>
+          <div className="text-left">
+            <form className="mb-16" onSubmit={startTournament}>
+              <h2 className="text-xl font-bold mb-6">Participants</h2>
+              <div className="flex flex-col w-1/2 mb-2">
+                {players.map((player) => (
+                  <label className="mb-3 flex justify-between" key={player}>
+                    {player}
+                    <input
+                      className="mx-3 rounded-md py-1 px-3"
+                      placeholder="Add a name"
+                      name={player}
+                      onChange={handleInputChange}
+                      value={values[player]}
+                    />
+                  </label>
+                ))}
+              </div>
+              <button>Start Tournament</button>
+            </form>
+          </div>
+        </section>
+      )}
+      {start && <Bracket players={values} />}
     </section>
   );
 }
--- src/components/Bracket/Bracket.jsx
+++ src/components/Bracket/Bracket.jsx
@@ -1,8 +1,11 @@
-function Bracket() {
+// 6:30 1.5 hours
+
+function Bracket({ values }) {
+  console.log(values);
   return (
     <section>
       <h1 className="mb-8">Tournament Bracket</h1>
-      <div className="w-[480px]">
+      <div className="w-full">
         <ul className="relative">
           <li className="relative mb-[100px]">
             <div className="p-2 border-amber-200 border-2 bg-amber-200 before:absolute before:border-t-2 before:w-full before:left-4 before:top-1/2 before:-z-10 after:absolute after:border-r-2 after:h-[100px] after:-right-4 after:top-1/2">
