1ab9388c32d2fcaebd460029457e175850d895ce
Matthew Banh <mattbanh92@gmail.com>
2023-01-08 00:23:40 -0800

    adjusted styling added ability to view bracket live with form changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 // If you use uuid, during re-render it will create a new key, re-rendering any elements that contain the key
 
 import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import Bracket from "./components/Bracket/Bracket";
 
 function App() {
@@ -31,19 +31,22 @@ function App() {
   };
 
   return (
-    <section>
-      {!start && (
-        <section>
-          <h1 className="mb-8">Start Your Tournament</h1>
+    <main className="m-16">
+      <section className="mx-auto max-w-7xl ">
+        <section className="">
+          <h1 className="text-lg mb-2">Start Your Tournament</h1>
           <div className="text-left">
             <form className="mb-16" onSubmit={startTournament}>
-              <h2 className="text-xl font-bold mb-6">Participants</h2>
-              <div className="flex flex-col w-1/2 mb-2">
+              <h2 className="text-md font-bold mb-2">Participants</h2>
+              <div className="flex flex-col mb-2">
                 {players.map((player) => (
-                  <label className="mb-3 flex justify-between" key={player}>
+                  <label
+                    className="mb-3 text-sm w-[240px] flex justify-between items-center"
+                    key={player}
+                  >
                     {player}
                     <input
-                      className="mx-3 rounded-md py-1 px-3"
+                      className="mx-3 rounded-md py-1 px-3 text-sm"
                       placeholder="Add a name"
                       name={player}
                       onChange={handleInputChange}
@@ -52,13 +55,13 @@ function App() {
                   </label>
                 ))}
               </div>
-              <button>Start Tournament</button>
+              <button className="text-sm">Build Bracket</button>
             </form>
           </div>
         </section>
-      )}
-      {start && <Bracket players={values} />}
-    </section>
+        <Bracket players={values} start={start} />
+      </section>
+    </main>
   );
 }
 
--- src/components/Bracket/Bracket.jsx
+++ src/components/Bracket/Bracket.jsx
@@ -1,6 +1,4 @@
-// 6:30 1.5 hours
-
-function Bracket({ players }) {
+function Bracket({ players, start }) {
   console.log(players);
   const getPlayerKeys = (players) => {
     let playerKeys = [];
@@ -13,19 +11,6 @@ function Bracket({ players }) {
     return playerKeys;
   };
 
-  // let playerNumber = 7;
-  // let playerArr = [];
-  // let rounds = Math.ceil(Math.log(playerNumber) / Math.log(2));
-
-  // for (let i = 0; i < playerNumber; i++) {
-  //   playerArr.push(`P${i + 1}`);
-  // }
-  // let eightGroup = (rounds - 2) * 8;
-  // while (playerArr.length < eightGroup) {
-  //   playerArr.push("BYE");
-  // }
-
-  // console.log(playerArr);
   const createSeedIndArr = (rounds) => {
     const iteration = rounds - 1;
 
@@ -74,44 +59,53 @@ function Bracket({ players }) {
     return sortedPlayerArr;
   };
 
+  const createMatches = (sortedPlayerKeys) => {
+    if (sortedPlayerKeys[0]) {
+      return sortedPlayerKeys.map((key, i) => {
+        if (i % 2 === 0) {
+          return (
+            <li key={(key, i)} className="relative mb-[40px]">
+              <div
+                className={
+                  (i + 2) % 4 !== 0
+                    ? "p-2  bg-green-200 before:absolute before:border-t-2 before:w-full before:left-4 before:top-1/2 before:-z-10 after:absolute after:border-r-2 after:h-[50px] after:-right-4 after:top-1/2"
+                    : "p-2  bg-green-200 before:absolute before:border-t-2 before:w-full before:left-4 before:top-1/2 before:-z-10 after:absolute after:border-r-2 after:h-[50px] after:-right-4 after:bottom-1/2"
+                }
+              >
+                <div className="flex flex-col text-left">
+                  <span className="text-black text-xs">{players[key]}</span>
+                  <span className="text-black text-xs">
+                    {sortedPlayerKeys[i + 1] === "BYE"
+                      ? sortedPlayerKeys[i + 1]
+                      : players[sortedPlayerKeys[i + 1]]}
+                  </span>
+                </div>
+              </div>
+            </li>
+          );
+        }
+      });
+    } else {
+      return <span className="text-sm">Please add players</span>;
+    }
+  };
+
   const createBracket = (players) => {
     const playerKeys = getPlayerKeys(players);
     const sortedPlayerKeys = sortPlayerKeys(playerKeys);
-    console.log("sPK", sortedPlayerKeys);
-    return sortedPlayerKeys.map((key, i) => {
-      if (i % 2 === 0) {
-        return (
-          <li key={(key, i)} className="relative mb-[100px]">
-            <div
-              className={
-                (i + 2) % 4 !== 0
-                  ? "p-2  bg-green-200 before:absolute before:border-t-2 before:w-full before:left-4 before:top-1/2 before:-z-10 after:absolute after:border-r-2 after:h-[100px] after:-right-4 after:top-1/2"
-                  : "p-2  bg-green-200 before:absolute before:border-t-2 before:w-full before:left-4 before:top-1/2 before:-z-10 after:absolute after:border-r-2 after:h-[100px] after:-right-4 after:bottom-1/2"
-              }
-            >
-              <div className="flex flex-col text-left">
-                <span className="text-black">{players[key]}</span>
-                <span className="text-black">
-                  {sortedPlayerKeys[i + 1] === "BYE"
-                    ? sortedPlayerKeys[i + 1]
-                    : players[sortedPlayerKeys[i + 1]]}
-                </span>
-              </div>
-            </div>
-          </li>
-        );
-      }
-    });
+    const matches = createMatches(sortedPlayerKeys);
+    return matches;
   };
-
-  return (
-    <section>
-      <h1 className="mb-8">Tournament Bracket</h1>
-      <div className="w-full">
-        <ul className="relative">{players && createBracket(players)}</ul>
-      </div>
-    </section>
-  );
+  if (start) {
+    return (
+      <section className="w-1/2">
+        <h1 className="mb-2 text-lg">Tournament Bracket</h1>
+        <div className="w-full">
+          <ul className="relative">{createBracket(players)}</ul>
+        </div>
+      </section>
+    );
+  }
 }
 
 export default Bracket;
